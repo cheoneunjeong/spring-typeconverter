32f53cd4402f9fb7337c935e8cec58df87f587a8
cheoneunjeong <0102skyjung@naver.com>
2024-06-20 17:45:05 +0900

    spring에 formatter 적용
    타입 -> 타입 동일한 구조가 converter, formatter 둘 다 있을 경우, converter 우선 순위가 높다.

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -2,6 +2,7 @@ package hello.typeconverter;
 
 import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import hello.typeconverter.type.IpPortToStringConverter;
 import hello.typeconverter.type.StringToIpPortConverter;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 타입 -> 타입 동일한 구조가 converter, formatter 둘 다 있을 경우, converter 우선 순위가 높다
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
